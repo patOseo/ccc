a8d5669294b32c8a109cb3561e7239dd90517961
Pat Monette <patrick@ontarioseo.ca>
2022-03-15 17:44:58 +0000

    add smooth scrolling

--- js/child-theme.js
+++ js/child-theme.js
@@ -19179,3 +19179,18 @@
 
 })));
 // Add your custom JS here.
+
+// Add your custom JS here.
+
+
+jQuery(function($){
+	
+  // Smooth anchor scrolling
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top -140
+	    }, 1000);
+	    return false;
+	});
+	
+});
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,1 +1,16 @@
 // Add your custom JS here.
+
+// Add your custom JS here.
+
+
+jQuery(function($){
+	
+  // Smooth anchor scrolling
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top -140
+	    }, 1000);
+	    return false;
+	});
+	
+});
